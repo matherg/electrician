0873f59ffa937e521e7f497ef1272aabdebe7169
Graham Mather <mather.g@northeastern.edu>
2024-07-12 13:44:05 -0400

    netlify

--- src/AlternatingSectionLeft.tsx
+++ src/AlternatingSectionLeft.tsx
@@ -10,8 +10,8 @@ const AlternatingSectionLeft = () => {
                         <img className="h-full w-full object-cover" src="/mountain.jpg" alt="Winding mountain road"/>
                     </div>
                     {/* Text Section */}
-                    <div className="max-w-lg bg-green md:max-w-2xl rounded-2xl md:z-10 md:shadow-lg md:absolute md:top-0 md:mt-48 lg:w-1/2 lg:right-0 lg:mt-20 lg:ml-20 xl:mt-24 xl:ml-12">
-                        <div className="flex flex-col p-12 md:px-16">
+                    <div className="bg-green md:max-w-2xl rounded-2xl md:z-10 md:shadow-lg md:absolute md:top-0 md:mt-48 lg:w-1/2 lg:right-0 lg:mt-20 lg:mr-20 xl:mt-24 xl:mr-12">
+                        <div className="flex flex-col p-8 py-10">
                             <h2 className="text-2xl font-medium  text-black lg:text-4xl">Lorem ipsum dolor sit</h2>
                             <p className="mt-4 text-white">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,8 +1,8 @@
 
 const Footer = () => {
     return (
-        <footer className="bg-blue w-full text-white py-6">
-            <div className="container mx-auto text-center">
+        <footer className="bg-blue w-full  text-white py-6">
+            <div className="container z-20 mx-auto text-center">
                 <p>© 2024 Electrician Company. All rights reserved.</p>
             </div>
         </footer>
