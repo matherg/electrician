84a05729a1ab29bf6d18b9796c5f1daa93410970
Graham Mather <mather.g@northeastern.edu>
2024-07-12 14:14:44 -0400

    header for mobile

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,10 +3,10 @@ const Header = () => {
         <header className="bg-white z-20 drop-shadow-lg text-blue w-full py-4 fixed top-0 left-0">
             <div className="container mx-auto flex items-center justify-between px-4 h-full" style={{ height: '6rem' }}>
                 <div className="flex items-center h-full">
-                    <img src="/jonlogo.png" alt="Company Logo" className="h-full mr-4 object-contain" />
+                    <img src="/jonlogo.png" alt="Company Logo" className="h-full hidden md:flex mr-4 object-contain" />
                     <span className="text-2xl font-bold">Ascent Electric</span>
                 </div>
-                <div className="hidden md:flex absolute left-1/2 transform -translate-x-1/2">
+                <div className="hidden md:flex items-center justify-center flex-grow">
                     <span className="text-2xl font-bold">Summit County, CO</span>
                 </div>
                 <nav className="flex flex-col items-end">
