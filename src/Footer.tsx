f62d4fb910eac32a5648b98674cdca987e8c616b
Graham Mather <mather.g@northeastern.edu>
2024-07-12 14:01:07 -0400

    Small screen works

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer className="bg-blue w-full  text-white py-6">
+        <footer className="bg-blue w-full text-white py-6">
             <div className="container flex justify-between z-20 mx-auto text-center">
                 <p>© 2024 Electrician Company</p>
                 <p>970-470-0191</p>
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,3 @@
-
-
 const Header = () => {
     return (
         <header className="bg-white z-20 drop-shadow-lg text-blue w-full py-4 fixed top-0 left-0">
@@ -8,16 +6,16 @@ const Header = () => {
                     <img src="/jonlogo.png" alt="Company Logo" className="h-full mr-4 object-contain" />
                     <span className="text-2xl font-bold">Ascent Electric</span>
                 </div>
-                <div className="absolute left-1/2 transform -translate-x-1/2">
+                <div className="hidden md:flex absolute left-1/2 transform -translate-x-1/2">
                     <span className="text-2xl font-bold">Summit County, CO</span>
                 </div>
-                <nav className="flex flex-col h-1/4 ">
-                    <ul className="flex text-xl space-x-8 ">
+                <nav className="flex flex-col items-end">
+                    <ul className="flex space-x-4 text-xl">
                         <li><a href="#" className="text-blue font-bold hover:text-grey">Home</a></li>
-                        <li><a href="#" className="text-blue font-bold  hover:text-grey">About</a></li>
+                        <li><a href="#" className="text-blue font-bold hover:text-grey">About</a></li>
                         <li><a href="#" className="text-blue font-bold hover:text-grey">Contact</a></li>
                     </ul>
-                    <p className="text-blue mt-2">ascentelectricinc@gmail.com</p>
+                    <p className="text-blue mt-2 text-sm md:text-base">ascentelectricinc@gmail.com</p>
                 </nav>
             </div>
         </header>
