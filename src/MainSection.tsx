73f45b19896a013b04e325bf0fcc4fbe968774a3
Graham Mather <mather.g@northeastern.edu>
2024-08-07 15:55:39 -0400

    wiring image

--- src/MainSection.tsx
+++ src/MainSection.tsx
@@ -6,7 +6,7 @@ const MainSection = () => {
         <div id="main" className="relative bg-bloo text-white w-full mt-10 min-h-[100vh] flex items-center justify-center">
             {/* Background Image with Overlay */}
             <div className="absolute top-0 left-0 w-full h-full bg-bloo opacity-80">
-                <img src="/mountain.jpg" alt="Background" className="w-full h-full object-cover" style={{ opacity: 0.2 }} />
+                <img src="/wiring.jpg" alt="Background" className="w-full h-full object-cover" style={{ opacity: 0.2 }} />
             </div>
             {/* Content */}
             <div className="text-center z-10">
