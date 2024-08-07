b72f3bad0d175cf5f060356f5dd432287b148a1b
Graham Mather <mather.g@northeastern.edu>
2024-08-07 15:53:02 -0400

    mountain bg

--- src/MainSection.tsx
+++ src/MainSection.tsx
@@ -1,11 +1,17 @@
 import {scrollToSection} from "./Header.tsx";
 
+
 const MainSection = () => {
     return (
-        <div id="main" className="bg-bloo text-white w-full mt-10 h-[80vh] flex items-center justify-center ">
-            <div className="text-center">
-                <h1 className="text-6xl text-white font-bold mb-4 ">Ascent Electric</h1>
-                <h2 className="text-5xl text-green font-bold mb-4 ">"Miles above the rest"</h2>
+        <div id="main" className="relative bg-bloo text-white w-full mt-10 min-h-[100vh] flex items-center justify-center">
+            {/* Background Image with Overlay */}
+            <div className="absolute top-0 left-0 w-full h-full bg-bloo opacity-80">
+                <img src="/mountain.jpg" alt="Background" className="w-full h-full object-cover" style={{ opacity: 0.2 }} />
+            </div>
+            {/* Content */}
+            <div className="text-center z-10">
+                <h1 className="text-6xl font-bold mb-4 text-white">Ascent Electric</h1>
+                <h2 className="text-5xl font-bold mb-4 text-green">"Miles above the rest"</h2>
                 <button onClick={() => scrollToSection('services')} className="bg-white text-bloo px-6 py-3 rounded-lg">Contact Us</button>
             </div>
         </div>
