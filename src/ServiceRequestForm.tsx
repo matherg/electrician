f2f70bc7447baaadaa98584de6108d49d92e4598
Graham Mather <mather.g@northeastern.edu>
2024-08-07 16:15:57 -0400

    trigger netlify

--- src/ServiceRequestForm.tsx
+++ src/ServiceRequestForm.tsx
@@ -32,7 +32,7 @@ const ServiceRequestForm = ({ openModal } : ServiceRequestFormProps) => {
             [name]: type === 'checkbox' ? checked : value
         });
     };
-
+    // net trigger
     const handleSubmit = async (e : any) => {
         e.preventDefault();
 
