055cd809f9aff689ed63d7d8064c4be468505ae4
Graham Mather <mather.g@northeastern.edu>
2024-08-07 15:46:25 -0400

    Most recent update

--- src/AlternatingSection.tsx
+++ src/AlternatingSection.tsx
@@ -1,24 +1,19 @@
 
-const AlternatingSection = () => {
+const AlternatingSection = ({ direction, imageSrc,bg, textColor, title, text }) => {
+    const isReversed = direction === 'right';
+
     return (
-        <div className="flex justify-end lg:px-20 w-full">
-            <div className="relative max-w-5xl mt-10  xl:max-w-6xl">
-                {/* Image and Text Wrapper */}
-                <div className="flex flex-col lg:flex-row-reverse justify-center  mx-auto">
-                    {/* Image Section */}
-                    <div className="w-full h-64 lg:w-1/2 lg:h-auto">
-                        <img className="h-full w-full rounded-2xl object-cover" src="/meet_the_team.jpg" alt="Winding mountain road"/>
-                    </div>
-                    {/* Text Section */}
-                    <div className=" bg-grey md:max-w-2xl rounded-2xl md:z-10 md:shadow-lg md:absolute md:top-0 md:mt-48 lg:w-1/2 lg:left-0 lg:mt-20 lg:ml-20 xl:mt-24 xl:ml-12">
-                        <div className="flex flex-col p-8 py-10 ">
-                            <h2 className="text-2xl font-semibold  text-black lg:text-3xl">Serving CO since 2001</h2>
-                            <p className="mt-4 text-white">
-                                Ascent electric is a professional electrical contracting service that has been operating in summit county colorado since 2001. Our service area covers Dillon, Silverthorne, Breckenridge, Frisco, and Eagle-vail.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        <div className={`flex ${isReversed ? 'flex-row-reverse' : 'flex-row'} justify-between items-center w-full lg:px-20 py-10`}>
+            {/* Image Section */}
+            <div className="w-full lg:w-1/2">
+                <img className="w-full h-auto rounded-2xl object-cover shadow-lg" src={imageSrc} alt="Team"/>
+            </div>
+            {/* Text Section */}
+            <div className={`w-full lg:w-1/2 lg:max-w-lg ${bg} ${textColor} p-8 mx-auto rounded-2xl shadow-lg`}>
+                <h2 className="text-2xl font-semibold lg:text-3xl">{title}</h2>
+                <p className="mt-4">
+                    {text}
+                </p>
             </div>
         </div>
     );
--- src/PictureSection.tsx
+++ src/PictureSection.tsx
@@ -1,13 +1,16 @@
 import AlternatingSection from "./AlternatingSection.tsx";
-import AlternatingSectionLeft from "./AlternatingSectionLeft.tsx";
-import AlternatingSectionBlue from "./AlternatingSectionBlue.tsx";
+
 
 const PictureSection = () => {
+    const text1 = "Our twenty years of experience have taught us how to do the job at a fair price and on time. We are confident that you will have a satisfying experience when you choose to use our services. It’s a promise we’ll keep!";
+    const text2 = "Having been in business for more than 20 years, we are the best in this field. So we know how to fix all of your electrical issues. If it’s to do with electricity we cover it. Commercial and residential!";
+    const text3 = "Ascent electric is a professional electrical contracting service that has been operating in summit county colorado since 2001. Our service area covers Dillon, Silverthorne, Breckenridge, Frisco, and Eagle-vail.";
     return (
         <div className="bg-gradient-to-b from-white from-5%  to-95% to-bloo w-full">
-            <AlternatingSection/>
-            <AlternatingSectionLeft/>
-            <AlternatingSectionBlue/>
+            <AlternatingSection text={text1} bg={"bg-bloo"} textColor={"text-white"}
+            direction={"right"} imageSrc={"/meet_the_team.jpg"}   title="Serving CO since 2001"/>
+            <AlternatingSection text={text2} bg={"bg-fadedbluegrey"} textColor={"text-white"} title={"Experience and Flexibility"} imageSrc={"/planning.jpg"} direction={"left"}/>
+            <AlternatingSection text={text3} bg={"bg-white"} textColor={"text-bloo"} title={"On Time and On Budget"} imageSrc={"/jon_level.jpg"} direction={"right"}/>
         </div>
     );
 };
