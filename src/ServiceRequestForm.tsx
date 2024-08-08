266cda3216b9ec93d0d500a91c33748d4ea54249
Graham Mather <mather.g@northeastern.edu>
2024-08-08 09:58:38 -0400

    rebase

--- src/ServiceRequestForm.tsx
+++ src/ServiceRequestForm.tsx
@@ -53,7 +53,7 @@ const ServiceRequestForm = ({ openModal } : ServiceRequestFormProps) => {
                 openModal(`Error: ${errorMessage}`);
             }
         } catch (error) {
-            // Handle unexpected errors
+            // Handle unexpected error
             console.error('Error submitting form:', error);
             openModal(`An unexpected error occurred`);
         }
