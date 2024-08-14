82fd7be75afaec9f258b47fb0f3f9183ae1eb2ee
Graham Mather <mather.g@northeastern.edu>
2024-08-13 21:13:36 -0400

    Image optimization

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer className="bg-bloo w-full text-white py-6">
             <div className="container flex justify-between z-20 mx-auto text-center">
-                <p>© 2024 Ascent Electric, INC. All Rights Reserved.</p>
+                <p>© 2024 Ascent Electric, Inc. All Rights Reserved.</p>
                 <p>970-470-0191</p>
             </div>
         </footer>
